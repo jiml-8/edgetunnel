e7357b37b42b1732e57f0b4fdce841ec72f3023c
jiml-8 <jimlewis5920@gmail.com>
2025-01-03 13:52:17 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,11 +1,11 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'a470a12a-eb5e-4a6e-b1ed-b77bf5b54764';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
-let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
+let subConfig = "https://raw.githubusercontent.com/gtang8/ACL4SSR/master/Clash/config/ACL4SSR_Mini.ini";
 let subProtocol = 'https';
 let subEmoji = 'true';
 let socks5Address = '';
