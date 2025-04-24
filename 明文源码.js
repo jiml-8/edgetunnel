b4184b17195de246cc18183980b41499ddc38970
jiml-8 <jimlewis5920@gmail.com>
2025-04-24 14:13:13 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'a470a12a-eb5e-4a6e-b1ed-b77bf5b54764';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
